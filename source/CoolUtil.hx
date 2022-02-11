8bd26055f07c95a292d0aea03a3813ec18ee57dc
Ilovepotatoes12 <99479611+Ilovepotatoes12@users.noreply.github.com>
2022-02-11 13:25:06 +0200

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -19,8 +19,8 @@ class CoolUtil
 	public static var defaultDifficulties:Array<String> = [
 		'Easy',
 		'Normal',
-		'Hard'
-	];
+		'Hard'.
+	];.     'Remix'
 	public static var defaultDifficulty:String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
 	public static var difficulties:Array<String> = [];
